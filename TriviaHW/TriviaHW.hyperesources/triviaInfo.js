aea70935a8cfeb0c41559f022757546d5d9c3512
Jesse Mattila <jesse.m.mattila@gmail.com>
2019-02-25 19:23:41 -0600

    HW1

---
+++ TriviaHW/TriviaHW.hyperesources/triviaInfo.js
@@ -0,0 +1,87 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"WORLD WAR II TRIVIA";
+	window.questions = [		"WHAT TWO COUNTRIES WHERE ALREADY IN CONFLICT BEFORE THE START OF THE WAR", 
+								"WHAT WAS THE LONGEST BATTLE OF WORLD WAR II",
+								"WHAT WAS THE FIRST NAZI CONCENTRATION CAMP", 
+								"IN WHAT BATTLE DID THE AXIS POWERS LOSE 1/4 OF THEIR TROOPS ON THE EASTERN FRONT", 
+								"WHAT WAS THE NAME OF THE B-29 BOMBER THAT DROPPED THE BOMB ON HIROSHIMA",
+								"WHAT COUNTRY LOST THE MOST LIVES IN WWII",
+								"WHAT WAS THE CODE NAME FOR THE BATTLE OF NORMANDY",
+								"WHAT WAS THE LARGEST BATTLESHIP OF THE WAR",
+								"WHAT BATTLE INVOLVED THE LARGEST ENGAGEMENT OF TANKS IN HISTORY",
+								"WHAT WAS THE LAST MAJOR BATTLE OF THE WAR" ];
+								
+	window.correctAnswer = [ 	"D", "C", "A", "B", "E", "B", "A", "C", "C", "E"]; 
+	window.answerA = [ 			"JAPAN & INDIA", 
+								"BATTLE OF MIDWAY", 
+								"DACHAU", 
+								"BATTLE OF LENINGRAD", 
+								"FAT MAN",
+								"UNITED STATES",
+								"OPERATION OVERLORD",
+								"MISSOURI",
+								"BATTLE OF MIDWAY",
+								"BATTLE OF BERLIN"
+								 ];
+								
+	window.answerB = [ 			"GERMANY & FRANCE", 
+								"BATTLE OF THE BULGE", 
+								"CHELMO", 
+								"BATTLE OF STALINGRAD",
+								"LITTLE BOY",
+								"SOVIET UNION",
+								"OPERATION PANTHER",
+								"MINNESOTA",
+								"BATTLE OF THE BULGE",
+								"BATTLE OF IWO JIMA"
+								 ];
+								
+	window.answerC = [ 			"GERMANY & POLAND",
+								"BATTLE OF THE ATLANTIC",
+								"RAVENSBRUCK", 
+								"BATTLE OF JUTLAND", 
+								"THUNDERBIRD",
+								"JAPAN",
+								"OPERATION PILEDRIVER",
+								"YAMATO",
+								"BATTLE OF KURSK",
+								"BATTLE OF JUTLAND" ];
+								
+	window.answerD = [ 			"JAPAN & CHINA",  
+								"BATTLE OF RIVER PLATE",
+								"AUSCHWITZ", 
+								"BATTLE OF KURSK",
+								"ROSA PARKS",
+								"AUSTRIA",
+								"OPERATION OMAHA",
+								"KONGO",
+								"BATTLE OF BERLIN",
+								"BATTLE OF PELELIU" ];
+								
+	window.answerE = [ 			"GERMAN & HOLLAND", 
+								"BATTLE AT KURSK", 
+								"HAVITZNACK", 
+								"BATTLE FOR MOSCOW", 
+								"ENOLA GAY",
+								"FRANCE",
+								"OPERATION FIRESTARTER",
+								"BISMARK",
+								"BATTLE OF NEWHOPE",
+								"BATTLE OF OKINAWA" ];
+								
+	window.response = [ 		"JAPAN AND CHINA WERE ALREADY AT WAR AT THE START OF WW2.", 
+								"LASTING FROM 1939 TO THE DEFEAT OF NAZI GERMANY IN 1945, THE BATTLE OF THE ATLANTIC WAS THE LONGEST CONTINUAL CAMPAIGN OF WW2", 
+								"FORMED IN AN EMPTY MUNITIONS FACTORY IN 1933, DACHAU WAS THE FIRST CONCENTRATION CAMP.", 
+								"THE BATTLE OF STALINGRAD WAS ONE OF THE BLOODIEST CAMPAIGNS IN HUMAN HISTORY.", 
+								"The Enola Gay, a B-29 Superfortress bomber piloted by Lt. Col. Paul Warfield Tibbets, Jr., was the plane that dropped the first atomic bomb on Hiroshima on August 6, 1945.",
+								"With approximately 27 Million total fatalities (both military and civilian), the Soviet Union lost the most lives during World War II.",
+								"Operation Overlord was launched on 6 June 1944 with the Normandy landings.",
+								"Yamato and her sister ship, Musashi, were the largest battleships ever built.",
+								"The Battle of Kursk involved as many as 6,000 tanks and is remembered as the greatest tank battle in history.",
+								"The Battle of Okinawa, which lasted from April 1, 1945 through June 21, 1945, was the last major battle of World War II." ];
+							
+}
+						
